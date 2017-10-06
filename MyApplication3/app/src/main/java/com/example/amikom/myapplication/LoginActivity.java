9e513a958d0be877d384874a8e6fbf97da8f6e2a
andiokta <andi.oktafiqurahman@students.amikom.ac.id>
2017-10-06 10:09:58 +0700

    Login

---
+++ MyApplication3/app/src/main/java/com/example/amikom/myapplication/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.amikom.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
+}
